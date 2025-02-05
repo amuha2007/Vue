6a87466f42b5990dfa81ee63e05fdd8b6366471c
Artem Mukha <amuha2007@gmail.com>
2025-02-05 21:19:58 +0300

    first commit

---
+++ lessons/src/components/Caruselcards.vue
@@ -0,0 +1,30 @@
+<script>
+
+import Card from './Card.vue';
+
+    export default {
+        components:{Card},
+        data(){
+            return{
+
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <h3>Товары со скидкой</h3>
+    <p>Наша компания предлагает покупать товар со скидкой не только в дни распродаж
+        или в течение действия ограниченных предложений, но и пользоваться скидками постоянно</p>
+    <div className="CaruselContainer">
+        <button><</button>
+            <Card/>
+        <button>></button>
+    </div>
+    <p>смотреть все</p>
+</template>
+
+<style>
+
+</style>
---
+++ lessons/src/components/Navigation/NavigationLinks/NavigationsLinks.vue
@@ -0,0 +1,28 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <div className="Links">
+        <p>Главная</p>
+        <p>Блог</p>
+        <p>Контакты</p>
+    </div>
+    
+</template>
+
+<style>
+    .Links{
+        display: flex;
+
+    }
+
+    .Links>p{
+        margin-left: 10px;
+        margin-right: 10px;
+        cursor: pointer;
+    }
+    
+</style>
