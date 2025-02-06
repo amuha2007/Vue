a0848c31eb5374aa81f314bc0948bde7226cb9c1
soroka41 <soroka41@gmail.com>
2025-02-06 10:16:57 +0300

    Add reviews

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <header>
+      <Navigation/>
+    </header>
+    <div className="Container" style="width: 80%; margin-left: auto; margin-right: auto;">
+        <MainPage/>
+    </div>
+
+    <footer>
+      <FooterContainer/>
+    </footer>
+</template>
+
+
+<style>
+  #app{
+    width: 80%;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  
+</style>
+<script>
+import Navigation from './components/Navigation/Navigation.vue';
+import FooterContainer from './components/Footer/FooterContainer.vue';
+import MainPage from './components/MainPage/MainPage.vue';
+
+export default{
+  components:{Navigation, FooterContainer, MainPage},
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
---
+++ src/components/OurInstagramm.vue
@@ -0,0 +1,12 @@
+<script>
+export default{
+
+}
+
+</script>
+
+<template>
+    <div><h3>Мы в Instagram</h3></div>
+</template>
+
+<style></style>
